419d41c67406083c07eab5bd1f73f5f97ab92ca6
oliviaalchemy <olivia@alchemytechs.com>
2019-08-07 12:31:42 +0100

    Add project files.

---
+++ For loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            
+              
+            }
+                    
+            Console.ReadLine(); //will print 4, 8, 15, 16, 23, 42
+        }
+    }
+}
